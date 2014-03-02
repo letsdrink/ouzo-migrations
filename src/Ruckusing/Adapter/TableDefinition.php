1352af2db54c98a853eb0e93e098ce734a2e0b2e
piotrooo <piotroo89@gmail.com>
2014-03-02 09:15:56 +0100

    cleanup

--- src/Ruckusing/Adapter/TableDefinition.php
+++ src/Ruckusing/Adapter/TableDefinition.php
@@ -1,84 +1,30 @@
 <?php
 namespace Ruckusing\Adapter;
-/**
- * Ruckusing
- *
- * @category  Ruckusing
- * @package   Ruckusing_Adapter
- * @author    Cody Caughlan <codycaughlan % gmail . com>
- * @link      https://github.com/ruckus/ruckusing-migrations
- */
+
 use Ruckusing\RuckusingException;
 
-/**
- * Ruckusing_Adapter_TableDefinition
- *
- * @category Ruckusing
- * @package  Ruckusing_Adapter
- * @author   Cody Caughlan <codycaughlan % gmail . com>
- * @link      https://github.com/ruckus/ruckusing-migrations
- */
 class TableDefinition
 {
-    /**
-     * columns
-     *
-     * @var array
-     */
     private $_columns = array();
 
     /**
-     * adapter
-     *
      * @var Base
      */
     private $_adapter;
 
-    /**
-     * Creates an instance of Ruckusing_Adapter_TableDefinition
-     *
-     * @param Base $adapter the current adapter
-     *
-     * @return Ruckusing_Adapter_TableDefinition
-     */
     public function __construct($adapter)
     {
         if (!($adapter instanceof Base)) {
-            throw new RuckusingException(
-                    'Invalid Adapter instance.',
-                    RuckusingException::INVALID_ADAPTER
-            );
+            throw new RuckusingException('Invalid Adapter instance.', RuckusingException::INVALID_ADAPTER);
         }
         $this->_adapter = $adapter;
     }
 
-    /**
-     * __call
-     *
-     * @param string $name The method name
-     * @param array  $args The parameters of method called
-     *
-     * @throws RuckusingException
-     */
     public function __call($name, $args)
     {
-        throw new RuckusingException(
-                'Method unknown (' . $name . ')',
-                RuckusingException::INVALID_MIGRATION_METHOD
-        );
+        throw new RuckusingException('Method unknown (' . $name . ')', RuckusingException::INVALID_MIGRATION_METHOD);
     }
 
-    /**
-     * Determine whether or not the given column already exists in our
-     * table definition.
-     *
-     * This method is lax enough that it can take either a string column name
-     * or a ColumnDefinition object.
-     *
-     * @param string $_column the name of the column
-     *
-     * @return boolean
-     */
     public function included($column)
     {
         $k = count($this->_columns);
@@ -95,11 +41,6 @@ class TableDefinition
         return false;
     }
 
-    /**
-     * Get list of columns
-     *
-     * @return string
-     */
     public function to_sql()
     {
         return join(",", $this->_columns);
