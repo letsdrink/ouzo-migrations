24c1032d139d19569f0d69ca768f552af5fc5596
piotrooo <piotroo89@gmail.com>
2014-03-21 10:04:17 +0100

    migrator refactor.

--- src/Task/Db/MigrateTask.php
+++ src/Task/Db/MigrateTask.php
@@ -12,9 +12,6 @@ use OuzoMigrations\Util\Naming;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-define('STYLE_REGULAR', 1);
-define('STYLE_OFFSET', 2);
-
 class MigrateTask implements TaskInterface
 {
     const OUZO_MIGRATIONS_SCHEMA_TABLE_NAME = 'schema_migrations';
@@ -148,7 +145,7 @@ class MigrateTask implements TaskInterface
                 $endTimer = $this->_endTimer();
 
                 $deltaTimer = $this->_deltaTimer($startTimer, $endTimer);
-                $this->_writeln(sprintf("========= %s ======== (%.2f)\n", $migrationFile->getClassName(), $deltaTimer));
+                $this->_writeln(sprintf("========= %s ========= (%.2f)\n", $migrationFile->getClassName(), $deltaTimer));
                 $lastVersion = $migrationFile->getVersion();
             }
         }
@@ -172,144 +169,15 @@ class MigrateTask implements TaskInterface
 
     private function _direction()
     {
-        $direction = self::MIGRATION_UP;
-        if ($this->_input->getArgument('version')) {
-        }
-        return $direction;
-    }
+        $targetVersion = $this->_input->getArgument('version');
+        $currentVersion = $this->_migrator->getMaxVersion();
 
-    public function exec($args)
-    {
-        if (!$this->_adapter->supportsMigrations()) {
-            throw new OuzoMigrationsException("This database does not support migrations.", OuzoMigrationsException::MIGRATION_NOT_SUPPORTED);
-        }
-        $this->_task_args = $args;
-        $this->_return .= "Started: " . date('Y-m-d g:ia T') . "\n\n";
-        $this->_return .= "[db:migrate]: \n";
-        try {
-            // Check that the schema_version table exists, and if not, automatically create it
-            $this->verify_environment();
-
-            $target_version = null;
-            $style = STYLE_REGULAR;
-
-            //did the user specify an explicit version?
-            if (array_key_exists('version', $this->_task_args)) {
-                $target_version = trim($this->_task_args['version']);
-            }
-
-            // did the user specify a relative offset, e.g. "-2" or "+3" ?
-            if ($target_version !== null) {
-                if (preg_match('/^([\-\+])(\d+)$/', $target_version, $matches)) {
-                    if (count($matches) == 3) {
-                        $direction = $matches[1] == '-' ? 'down' : 'up';
-                        $steps = intval($matches[2]);
-                        $style = STYLE_OFFSET;
-                    }
-                }
-            }
-            //determine our direction and target version
-            $current_version = $this->_migrator_util->get_max_version();
-            if ($style == STYLE_REGULAR) {
-                if (is_null($target_version)) {
-                    $this->_prepareToMigrate($target_version, 'up');
-                } elseif ($current_version > $target_version) {
-                    $this->_prepareToMigrate($target_version, 'down');
-                } else {
-                    $this->_prepareToMigrate($target_version, 'up');
-                }
-            }
-
-            if ($style == STYLE_OFFSET) {
-                $this->migrate_from_offset($steps, $current_version, $direction);
-            }
-
-            // Completed - display accumulated output
-            if (!empty($output)) {
-                $this->_return .= "\n\n";
-            }
-        } catch (OuzoMigrationsException $ex) {
-            if ($ex->getCode() == OuzoMigrationsException::MISSING_SCHEMA_INFO_TABLE) {
-                $this->_return .= "\tSchema info table does not exist. I tried creating it but failed. Check permissions.";
-            } else {
-                throw $ex;
-            }
-        }
-        $this->_return .= "\n\nFinished: " . date('Y-m-d g:ia T') . "\n\n";
-
-        return $this->_return;
-    }
-
-    private function migrate_from_offset($steps, $current_version, $direction)
-    {
-        $migrations = $this->_migrator_util->get_migration_files($this->_migratorDirs, $direction);
-
-        $current_index = $this->_migrator_util->find_version($migrations, $current_version, true);
-        $current_index = $current_index !== null ? $current_index : -1;
-
-        if ($this->_debug == true) {
-            $this->_return .= print_r($migrations, true);
-            $this->_return .= "\ncurrent_index: " . $current_index . "\n";
-            $this->_return .= "\ncurrent_version: " . $current_version . "\n";
-            $this->_return .= "\nsteps: " . $steps . " $direction\n";
-        }
-
-        // If we are not at the bottom then adjust our index (to satisfy array_slice)
-        if ($current_index == -1 && $direction === 'down') {
-            $available = array();
+        if (!$targetVersion) {
+            return self::MIGRATION_UP;
+        } elseif ($currentVersion > $targetVersion) {
+            return self::MIGRATION_DOWN;
         } else {
-            if ($direction === 'up') {
-                $current_index += 1;
-            } else {
-                $current_index += $steps;
-            }
-            // check to see if we have enough migrations to run - the user
-            // might have asked to run more than we have available
-            $available = array_slice($migrations, $current_index, $steps);
-        }
-
-        $target = end($available);
-        if ($this->_debug == true) {
-            $this->_return .= "\n------------- TARGET ------------------\n";
-            $this->_return .= print_r($target, true);
-        }
-        $this->_prepareToMigrate(isset($target['version']) ? $target['version'] : null, $direction);
-    }
-
-    private function verify_environment()
-    {
-        if (!$this->_adapter->tableExists(RUCKUSING_TS_SCHEMA_TBL_NAME)) {
-            $this->_return .= "\n\tSchema version table does not exist. Auto-creating.";
-            $this->auto_create_schema_info_table();
-        }
-
-        $this->_migratorDirs = $this->get_framework()->migrations_directories();
-
-        // create the migrations directory if it doesnt exist
-        foreach ($this->_migratorDirs as $name => $path) {
-            if (!is_dir($path)) {
-                $this->_return .= sprintf("\n\tMigrations directory (%s) doesn't exist, attempting to create.", $path);
-                if (mkdir($path, 0755, true) === FALSE) {
-                    $this->_return .= sprintf("\n\tUnable to create migrations directory at %s, check permissions?", $path);
-                } else {
-                    $this->_return .= sprintf("\n\tCreated OK");
-                }
-            }
-            //check to make sure our destination directory is writable
-            if (!is_writable($path)) {
-                throw new OuzoMigrationsException("ERROR: Migrations directory '" . $path . "' is not writable by the current user. Check permissions and try again.\n", OuzoMigrationsException::INVALID_MIGRATION_DIR);
-            }
-        }
-    }
-
-    private function auto_create_schema_info_table()
-    {
-        try {
-            $this->_return .= sprintf("\n\tCreating schema version table: %s", RUCKUSING_TS_SCHEMA_TBL_NAME . "\n\n");
-            $this->_adapter->createSchemaVersionTable();
-            return true;
-        } catch (Exception $e) {
-            throw new OuzoMigrationsException("\nError auto-creating 'schema_info' table: " . $e->getMessage() . "\n\n", OuzoMigrationsException::MIGRATION_FAILED);
+            return self::MIGRATION_UP;
         }
     }
 }
